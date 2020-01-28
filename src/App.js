92974470c5a496e1191c996050060f83f822840d
Petr Cibulka <cibulka.me@gmail.com>
2020-01-28 13:53:10 +0100

    readme and screenshot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typed from 'react-typed';
+
+import { Thing } from 'tsdx-test';
+
+const style = {
+  position: 'fixed', top: 0, left: 0, width: '100%', height: '100%',
+  display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column',
+};
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={style}>
+        <h1>
+            <Typed strings={['Hello world!']} />
+        </h1>
+        <br />
+        <Thing />
+      </div>
   );
 }
 
